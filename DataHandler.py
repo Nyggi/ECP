Implemented scaling of values and fixed evaluation to work with multiple outputs

DataHandler.py
@@ -1,5 +1,7 @@
 import mysql.connector
 from collections import namedtuple
+from sklearn.preprocessing import MinMaxScaler
+import numpy as np
 
 HourData = namedtuple('HourData', ['house_id', 'timestamp', 'consumption'])
 
@@ -11,7 +13,14 @@ class DataHandler:
         self.house_id = house_id
         self.data = self._fetch_data(house_id)
 
-        train_input, train_labels, eval_input, eval_labels = self._construct_training_data(self.data)
+        self.scaler = MinMaxScaler(feature_range=(0, 1))
+
+        if cfg.SCALE_VALUES is True:
+            scaled_data = self._get_scaled_data()
+            train_input, train_labels, eval_input, eval_labels = self._construct_training_data(scaled_data)
+        else:
+            self.scaler = None
+            train_input, train_labels, eval_input, eval_labels = self._construct_training_data(self.data)
 
         self.train_input = train_input
         self.train_labels = train_labels
@@ -102,3 +111,19 @@ class DataHandler:
             consumptions.append(d.consumption)
 
         return consumptions
+
+    def _get_scaled_data(self):
+        consumption_list = list()
+
+        for i in self.data:
+            consumption_list.append([i.consumption])
+
+        consumption_list = np.asarray(consumption_list)
+        scaled_consumption = self.scaler.fit_transform(consumption_list)
+
+        scaled_data = list()
+
+        for j in range(len(self.data)):
+            scaled_data.append(HourData(self.data[j].house_id, self.data[j].timestamp, scaled_consumption[j][0]))
+
+        return scaled_data

Tools.py
@@ -1,23 +1,48 @@
 import numpy as np
 from keras.models import clone_model
+import matplotlib.pyplot as plt
 
 
-def fit_model(cfg, model, train_input, train_labels):
+def fit_model(cfg, model, train_input, train_labels, show_error_graph=False):
     best_mape = 999999999
 
     best_model = clone_model(model)
-    best_model.compile(loss=cfg.LOSS, optimizer=cfg.OPTIMIZER, metrics=["mape"])
+    best_model.compile(loss=cfg.LOSS, optimizer=cfg.OPTIMIZER, metrics=["mape", "mae"])
+
+    mape_list = list()
+    mae_list = list()
 
     for e in range(cfg.EPOCHS):
         stats = model.fit(np.array(train_input), np.array(train_labels), cfg.BATCH_SIZE, 1, verbose=0, shuffle=True)
         mape = stats.history['mean_absolute_percentage_error'][0]
+        mae = stats.history['mean_absolute_error'][0]
 
-        print("Epoch " + str(e) + '/' + str(cfg.EPOCHS) + ' - MAPE: ' + str(mape))
+        mape_list.append(mape)
+        mae_list.append(mae)
+
+        print("Epoch " + str(e + 1) + '/' + str(cfg.EPOCHS) + ' - MAPE: ' + str(mape))
 
         if mape < best_mape:
             best_mape = mape
             # Copy weigths from model to best model
             best_model.set_weights(model.get_weights())
 
-    return best_model
+    if show_error_graph is True:
+        fig, ax1 = plt.subplots()
+        mape_plot = ax1.plot(mape_list, label='MAPE', color='blue')
 
+        ax2 = ax1.twinx()
+        mae_plot = ax2.plot(mae_list, label='MAE', color='red')
+
+        lns = mape_plot + mae_plot
+        labs = [l.get_label() for l in lns]
+        ax1.legend(lns, labs, loc=0)
+
+        ax1.set_xlabel('Epochs')
+        ax1.set_ylabel('MAPE')
+        ax2.set_ylabel('MAE')
+
+        fig.tight_layout()
+        plt.show()
+
+    return best_model